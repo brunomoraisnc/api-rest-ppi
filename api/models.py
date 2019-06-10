f7e9680d2558b7d0dde9f9b17a71b93f10e02fa0
Bruno Morais <brunomoraisnc@gmail.com>
2019-06-10 17:43:58 -0300

    Conserta nova modelagem

--- api/models.py
+++ api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class ApiItem(models.Model):
-    cpf = models.CharField(max_length=11, default='')
-    mac = models.CharField(max_length=17, default='')
-    coords = models.CharField(max_length=25, default='') 
+    cpf = models.CharField(max_length=11)
+    mac = models.CharField(max_length=17)
+    coords = models.CharField(max_length=25) 
     datetime = models.DateTimeField(auto_now_add=True)
-    presenca = models.CharField(max_length=1, default=0)
+    presenca = models.CharField(max_length=1)
