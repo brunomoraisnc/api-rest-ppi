539c10187c04a4db4a04fb6525d03c8c853095a7
Bruno Morais <brunomoraisnc@gmail.com>
2019-06-10 17:35:22 -0300

    Ajusta modelagem

--- api/models.py
+++ api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class ApiItem(models.Model):
-    cpf = models.CharField(max_length=11)
-    mac = models.CharField(max_length=17)
-    coords = models.CharField(max_length=25) 
+    cpf = models.CharField(max_length=11, default='')
+    mac = models.CharField(max_length=17, default='')
+    coords = models.CharField(max_length=25, default='') 
     datetime = models.DateTimeField(auto_now_add=True)
-    presenca = models.BooleanField(default=False)
+    presenca = models.CharField(max_length=1, default=0)
