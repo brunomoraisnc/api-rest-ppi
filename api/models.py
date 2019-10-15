a99d300e366060b439cc49704d99e29cf3398378
Bruno Morais <brunomoraisnc@gmail.com>
2019-10-14 23:10:49 -0300

    Atualiza modelagem de dados

--- api/models.py
+++ api/models.py
@@ -1,11 +1,28 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
+# class Person(models.Model):
+#     cpf = models.CharField(max_length=11)
+#     uid = models.CharField(max_length=17)
+
 class Location(models.Model):
-    cpf = models.CharField(max_length=11)
-    uid = models.CharField(max_length=17)
-    coords = models.CharField(max_length=25)
-    date = models.DateTimeField(auto_now_add=True)
-    datetime = models.DateTimeField(auto_now_add=True)
-    presenca = models.CharField(max_length=1)
+    # person = models.ForeignKey(
+    #         'Person',
+    #         on_delete=models.CASCADE,
+    #         default='1'
+    #     )
+    cpf = models.CharField(max_length=11, null=False, default='0')
+    uid = models.CharField(max_length=17, null=False, default='0')
+    provider = models.CharField(max_length=28,  default='undefined')
+    locationProvider = models.CharField(max_length=1, null=True, default=9)
+    timestamp = models.FloatField(null=False, default=0)
+    latitude = models.FloatField(null=False, default=0)
+    longitude = models.FloatField(null=False, default=0)
+    accuracy = models.FloatField(null=True)
+    speed = models.FloatField(null=True)
+    altitude = models.FloatField(null=True)
+    bearing = models.IntegerField(null=True)
+    isFromMockProvider = models.BooleanField(null=True)
+    mockLocationsEnabled = models.BooleanField(null=True)
