d5df41724a534a368e4dc57d6231f64058a2cf66
Bruno Morais <brunomoraisnc@gmail.com>
2019-10-20 22:15:14 -0300

    Ajusta models.py

--- api/models.py
+++ api/models.py
@@ -16,14 +16,14 @@ class Location(models.Model):
     #         default='1'
     #     )
     # locations = JSONField()
-    cpf = models.CharField(max_length=11, null=False, default='0'),
-    uid = models.CharField(max_length=17, null=False, default='0'),
-    provider = models.CharField(max_length=28,  default='undefined'),
-    locationProvider = models.CharField(max_length=1, null=True, default=9),
-    timestamp = models.FloatField(null=False, default=0),
-    latitude = models.FloatField(null=False, default=0),
-    longitude = models.FloatField(null=False, default=0),
-    accuracy = models.FloatField(null=True),
-    speed = models.FloatField(null=True),
-    altitude = models.FloatField(null=True),
+    cpf = models.CharField(max_length=11, null=False, default='0')
+    uid = models.CharField(max_length=17, null=False, default='0')
+    provider = models.CharField(max_length=28,  default='undefined')
+    locationProvider = models.CharField(max_length=1, null=True, default=9)
+    timestamp = models.FloatField(null=False, default=0)
+    latitude = models.FloatField(null=False, default=0)
+    longitude = models.FloatField(null=False, default=0)
+    accuracy = models.FloatField(null=True)
+    speed = models.FloatField(null=True)
+    altitude = models.FloatField(null=True)
     bearing = models.IntegerField(null=True)
