662e87bad2e6a0ab35df0571c01bf2946aa07715
Bruno Morais <brunomoraisnc@gmail.com>
2019-10-20 21:27:49 -0300

    Teste

--- api/models.py
+++ api/models.py
@@ -15,9 +15,7 @@ class Location(models.Model):
     #         on_delete=models.CASCADE,
     #         default='1'
     #     )
-    locations = ArrayField(
-        JSONField()
-    )
+    locations = JSONField()
     # cpf = models.CharField(max_length=11, null=False, default='0'),
     # uid = models.CharField(max_length=17, null=False, default='0'),
     # provider = models.CharField(max_length=28,  default='undefined'),
