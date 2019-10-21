4f3cd783893c8d8057f2e51c6d8b4aadc5a9c24d
Bruno Morais <brunomoraisnc@gmail.com>
2019-10-20 21:16:29 -0300

    Altera serializer

--- api/views.py
+++ api/views.py
@@ -13,17 +13,6 @@ class LocationViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.G
     serializer_class = LocationSerializer
     search_fields = ('cpf','latitude')
     
-    def create(self, request, *args, **kwargs):
-        """
-        #checks if post request data is an array initializes serializer with many=True
-        else executes default CreateModelMixin.create function 
-        """
-        is_many = isinstance(request.data, list)
-        if not is_many:
-            return super(LocationViewSet, self).create(request, *args, **kwargs)
-        else:
-            serializer = self.get_serializer(data=request.data, many=True)
-            serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
-            headers = self.get_success_headers(serializer.data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    def get_serializer(self, instance=None, data=None,
+                    files=None, many=True, partial=False):
+        return super(LocationViewSet, self).get_serializer(instance, data, files, many, partial)
