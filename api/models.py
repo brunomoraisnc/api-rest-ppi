c7be2e3cb3d603e7ed2d95648c1097d75fe41079
Bruno Morais <brunomoraisnc@gmail.com>
2019-10-20 21:48:52 -0300

    Teste2

--- api/models.py
+++ api/models.py
@@ -15,17 +15,17 @@ class Location(models.Model):
     #         on_delete=models.CASCADE,
     #         default='1'
     #     )
-    locations = JSONField()
-    # cpf = models.CharField(max_length=11, null=False, default='0'),
-    # uid = models.CharField(max_length=17, null=False, default='0'),
-    # provider = models.CharField(max_length=28,  default='undefined'),
-    # locationProvider = models.CharField(max_length=1, null=True, default=9),
-    # timestamp = models.FloatField(null=False, default=0),
-    # latitude = models.FloatField(null=False, default=0),
-    # longitude = models.FloatField(null=False, default=0),
-    # accuracy = models.FloatField(null=True),
-    # speed = models.FloatField(null=True),
-    # altitude = models.FloatField(null=True),
-    # bearing = models.IntegerField(null=True),
-    # isFromMockProvider = models.BooleanField(null=True),
-    # mockLocationsEnabled = models.BooleanField(null=True)
+    # locations = JSONField()
+    cpf = models.CharField(max_length=11, null=False, default='0'),
+    uid = models.CharField(max_length=17, null=False, default='0'),
+    provider = models.CharField(max_length=28,  default='undefined'),
+    locationProvider = models.CharField(max_length=1, null=True, default=9),
+    timestamp = models.FloatField(null=False, default=0),
+    latitude = models.FloatField(null=False, default=0),
+    longitude = models.FloatField(null=False, default=0),
+    accuracy = models.FloatField(null=True),
+    speed = models.FloatField(null=True),
+    altitude = models.FloatField(null=True),
+    bearing = models.IntegerField(null=True),
+    isFromMockProvider = models.BooleanField(null=True),
+    mockLocationsEnabled = models.BooleanField(null=True)
--- api/views.py
+++ api/views.py
@@ -13,17 +13,29 @@ class LocationViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.G
     serializer_class = LocationSerializer
     search_fields = ('cpf','latitude')
     
-    def create(self, request, *args, **kwargs):
-        """
-        #checks if post request data is an array initializes serializer with many=True
-        else executes default CreateModelMixin.create function 
-        """
-        is_many = isinstance(request.data, list)
+    def post(self, request, *args, **kwargs):
+        urls = request.data
+
+        is_many = isinstance(urls, list)
         if not is_many:
             return super(LocationViewSet, self).create(request, *args, **kwargs)
         else:
-            serializer = self.get_serializer(data=request.data, many=True)
+            serializer = self.get_serializer(data=urls, many=True)
             serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
-            headers = self.get_success_headers(serializer.data)
+            self.create_list(serializer)
             return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    # def create(self, request, *args, **kwargs):
+    #     """
+    #     #checks if post request data is an array initializes serializer with many=True
+    #     else executes default CreateModelMixin.create function 
+    #     """
+    #     is_many = isinstance(request.data, list)
+    #     if not is_many:
+    #         return super(LocationViewSet, self).create(request, *args, **kwargs)
+    #     else:
+    #         serializer = self.get_serializer(data=request.data, many=True)
+    #         serializer.is_valid(raise_exception=True)
+    #         self.perform_create(serializer)
+    #         headers = self.get_success_headers(serializer.data)
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
