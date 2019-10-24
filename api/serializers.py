1bb1627c6ab5549c93e24e4d3b032924cf15d139
Bruno Morais <brunomoraisnc@gmail.com>
2019-10-23 21:27:03 -0300

    Atualiza serializers

--- api/serializers.py
+++ api/serializers.py
@@ -3,13 +3,13 @@ from rest_framework import serializers
 from .models import Location
 
 
-class LocationsSerializer(serializers.ModelSerializer):
+class LocationSerializer(serializers.ModelSerializer):
     model = Location
     fields = '__all__'
 
 class LocationSerializer(serializers.Serializer):
-    locations = LocationsSerializer(many=True)
-    # class Meta:
-        
+    
+    class Meta:
+        locations = LocationSerializer(many=True)
         # model = Location
         # fields = '__all__'
