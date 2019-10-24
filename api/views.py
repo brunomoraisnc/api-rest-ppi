b4403bf17d9e975281b0a0021dd0b4a12eb81515
Bruno Morais <brunomoraisnc@gmail.com>
2019-10-23 21:44:34 -0300

    Corrige typo

--- api/views.py
+++ api/views.py
@@ -14,7 +14,7 @@ class LocationsViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.
     queryset = Location.objects.all()
     print(queryset)
     serializer_class = LocationsSerializer(queryset, many=True).data
-    print([for i in serializer_class])
+    print([i for i in serializer_class])
     # search_fields = ('cpf','latitude')
     
     def post(self, request, *args, **kwargs):
